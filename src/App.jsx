0afad24fcba499456820d794def74e26f4b0e337
ilnaz valeev <alexsanderkyznecov@mail.ru>
2025-05-15 00:58:48 +0500

    Fix routing and deploy to GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { subjects } from "./data/data"; // Импортируем данные из data.js
 import Home from "./pages/Home"; // Главная страница
 import Topics from "./pages/Topics"; // Страница выбора темы
@@ -8,24 +8,22 @@ import Test from "./pages/Test"; // Страница с тестом
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        {/* Главная страница */}
-        <Route path="/" element={<Home />} />
+    <Routes>
+      {/* Главная страница */}
+      <Route path="/" element={<Home />} />
 
-        {/* Страница выбора темы */}
-        <Route
-          path="/test/:subjectName"
-          element={<Topics subjects={subjects} />}
-        />
+      {/* Страница выбора темы */}
+      <Route
+        path="/test/:subjectName"
+        element={<Topics subjects={subjects} />}
+      />
 
-        {/* Страница с тестом по теме */}
-        <Route
-          path="/test/:subjectName/:topicName"
-          element={<Test subjects={subjects} />} // Передаем данные в Test через пропсы
-        />
-      </Routes>
-    </Router>
+      {/* Страница с тестом по теме */}
+      <Route
+        path="/test/:subjectName/:topicName"
+        element={<Test subjects={subjects} />} // Передаем данные в Test через пропсы
+      />
+    </Routes>
   );
 };
 
